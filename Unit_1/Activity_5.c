34e34fcd6e7120d7fed8a3dee4f47ff5a507e619
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-03-14 09:16:02 -0600

    Activity 5

---
+++ Unit_1/Activity_5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    printf("you enter a name as:%s\n", argv[1]);
+    printf("you enter a last name as:%s\n", argv[2]);
+    printf("you enter an age as:%s\n", argv[3]);
+    printf("you enter a state as:%s\n", argv[4]);
+    return 0;
+}
