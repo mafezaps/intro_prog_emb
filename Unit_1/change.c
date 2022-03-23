4b18e33db687e3d92c4de75623794b603cc95501
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-03-23 00:00:00 -0600

    inputs_

---
+++ Unit_1/change.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+
+
+int main(int argc, char** argv){
+    printf("argc[0]: %s, argv1: %s, argv2: %s, argv3: %s, argv4: %s\n", argv[0], argv[1], argv[2], argv[3], argv[4]);
+
+    return 0;
+}
