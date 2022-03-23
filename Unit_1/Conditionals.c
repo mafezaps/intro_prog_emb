500573685022ccfba40f909f5a1d6b0a44cbffdf
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-03-22 23:40:12 -0600

    Conditionals exercise

---
+++ Unit_1/Conditionals.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+
+    int a = 10;
+    int b = 15;
+    int c = 1;
+    int d = 8;
+    int e = 10;
+
+    bool condicion1 = (a>b); //false
+    bool condicion2 = (c<d); //true
+    bool condicion3 = (d==a); //false
+    bool condicion4 = (d==b); //false
+    bool condicion5 = (a>=b); //false
+    bool condicion6 = (e>=a); //true
+    
+
+    int number1 = 3;
+    bool isGreaterThanOne = (number1 > 1);
+    if (!isgreaterthanone){
+    }else{
+
+    }
+    return 0
+}
