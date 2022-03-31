9d5d7b6d6791c0d852f26d7acd8694e5fa3a8e6b
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-03-31 07:52:11 -0600

    Loops
    Loops activity

---
+++ Unit2_/loops.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#include <stdlib.h>
+int main(int argc, char**argv)
+{
+    printf("a number string: %s\n", argv[1]);
+    char*numberstring = argv[1];
+    int number = atoi(numberstring);
+        if (number%2 ==0){
+            printf("es un número entero");
+        }else{
+            printf("es un número impar");
+        }
+
+return 0;
+}
