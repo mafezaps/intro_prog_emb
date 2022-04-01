b8b0e1a96f94807b1fb4ec74e1e8f9a859390fed
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-04-01 02:43:15 -0600

    array2
    array2 activity

---
+++ Unit2_/array2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+    int myVar = 25;
+            //tamaño
+    float alturas[4] = {1.70, 1.80, 1.50, 1.68};
+    //            index   0     1     2     3
+    return 0;
+
+    printf("el valor de arr[2]", argv[2]);
+}
