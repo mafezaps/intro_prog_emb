9a3d29de5851ebff65c7cc8914746ae8c5aa1f1e
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-04-01 02:42:45 -0600

    Arrays
    arrays activity

---
+++ Unit2_/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int argc , char** argv){
+    
+    int array = atoi(argv[1]);
+    int array2 = atoi(argv[2]);
+    int array3 = atoi(argv[3]);
+    
+    printf("array1: %d\n", array);
+    printf("array2: %d\n", array2);
+    printf("array3: %d\n", array3);
+    
+   
+        
+    return 0;
+}
