dee45a9468e696ebb2346fa85c8aa0cb339dce94
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-04-03 15:11:18 -0500

    Update even_or_odd_number.c

--- Unit2_/even_or_odd_number.c
+++ Unit2_/even_or_odd_number.c
@@ -1,19 +1,24 @@
-#include <studio.h>
-#include <stdlib.h>
-int main(int argc, char**argv)
+#include <stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-{
-    printf("A number string: %s\n", argv[1]);
-    char* numberstring= argv[1];
-    int number= atoi(numberstring);
-        if (number%2==0){
-            printf("Es un número entero");
-        }else{
-            printf("Es un número impar");
+int main(int argc , char** argv){
+    
+    int number = atoi(argv[1]);
+
+    
+        while (number<=10){
+           number = number+1;
+            
+            for (number ;number <= 5; number = number + 1){
+                if(number%2==0){
+                    printf("you enter a even number:%i\n",number);
+                }else{
+                    printf("you enter an odd number :%i\n",number);
+                };
+            };
         }
 
-
-
-
-return 0
+        
+    return 0;
 }
