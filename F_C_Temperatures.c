2572ba893e1ec23a412db7c1d644649c0bab9df6
Fernanda Zapata <98876860+mafezaps@users.noreply.github.com>
2022-04-07 04:51:50 -0500

    Add files via upload

---
+++ F_C_Temperatures.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int dataArray[length];
+int dataArray[tempF] = [63, 73, -999, 91, 86, 82, 72, 81, 66, 77, 75, 104, -999, 84, 77, 66, 82, 63, 93, 72, 82, 64, 75 , 23.88, 64, -999, 99, 82, 95, 82];
+int dataArray[tempC] = [17.22, 22.77, -572.77, 32.77, 30, 27.77, 22.22,27.22, 18.88, 25, 23.88, 40, -572.77, 28.88, 25, 18.88, 27.77, 17.222;, 33.88,22.22, 27.77, 17.77, 23.88, 17.77, -572.77, 99, 37.22, 27.77, 27.7735)
+.
+.
+printf("Enter tempF\n");
+    scanf("%d temF is the greatest";
+    if("temF>tempC"){
+
+        printf("%d is  greater");
+    }
+
+    if("temC>tempF")
+    
+    }else{ ("%d is greater",);
+    printf("%d is greater");
+
+return 0;
+
+}
